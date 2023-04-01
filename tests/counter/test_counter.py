41c36cf61c268468a870e816b3dd399032c2d529
Caio <caiusito@gmail.com>
2023-04-01 11:56:56 -0300

    Req 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,7 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    test_counter_js = count_ocurrences('data/jobs.csv', r'/javascript/')
+    test_counter_py = count_ocurrences('data/jobs.csv', r'/python/')
+    assert test_counter_js != 0 and test_counter_py != 0
