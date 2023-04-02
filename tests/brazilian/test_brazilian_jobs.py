ec87864d7d00cfad7cb6e979b1d2ac48f2427b87
Caio <caiusito@gmail.com>
2023-04-02 18:05:02 -0300

    Req 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,12 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    brazilian_translation = read_brazilian_file(
+        "tests/mocks/brazilians_jobs.csv"
+        )
+    assert brazilian_translation[1] == {
+            "salary": "3000",
+            "title": "Motorista",
+            "type": "full time",
+    }
